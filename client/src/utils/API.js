fbd8071ee0ae5943c9333510cefbeccdb3548491
Dylan Best <best.dylan15@gmail.com>
2020-11-16 20:12:09 -0500

    models. controllers, api utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,4 +5,16 @@ export default {
   searchBooks: function(searchInput) {
     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchInput}`);
   },
+  // gets saved books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // creates new saved book
+  saveBook: function(bookData) {
+    return axios.get("/api/books", bookData);
+  },
+  // deletes saved book
+  deleteBook: function(id) {
+    return axios.get(`/api/books/${id}`);
+  }
 }
--- routes/api/books.js
+++ routes/api/books.js
@@ -2,7 +2,12 @@ const router = require("express").Router();
 const bookController = require("../../controllers/bookController");
 
 // Matches with "/api/books"
-router
-  .route("/");
+router.route("/")
+  .get(bookController.findAll)
+  .post(bookController.create);
+
+// Matches with "/api/books/:id"
+router.route("/:id")
+  .delete(bookController.remove);
 
 module.exports = router;
